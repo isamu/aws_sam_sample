a2c97d21429d6bd530dbfa87ae3b77695e5db9aa
Isamu Arimoto <isamu@to-kyo.to>
2018-10-04 04:00:19 +0900

    add sample

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const lambdaRouter = require ("lambda-router");
+
+const testController = require ("controllers/test")
+
+lambdaRouter.setInit((event, context) => {
+  // some init function
+});
+
+lambdaRouter.setRoutes([
+  {method: "GET", path: "test", func: testController.test},
+  {method: "POST", path: "hello/:message", func: testController.hello }, 
+]);
+
+lambdaRouter.setResponseHandlers({
+  400: testController.errorHandler(400),
+  401: testController.errorHandler(401),
+  404: testController.errorHandler(404),
+})
+
+lambdaRouter.setErrorCallback((event, error) => {
+  console.log(error);
+}); 
+
+const router = lambdaRouter.router
+  
+module.exports = {
+  router,
+}
+
---
+++ src/controllers/test.js
@@ -0,0 +1,36 @@
+const test = (event, context, callback) => {
+  const response = {
+    'statusCode': 200,
+    'body': JSON.stringify({
+      message: 'test',
+    }),
+  };
+  return callback(null, response)
+};
+
+const hello = (event, context, callback) => {
+  const response = {
+    'statusCode': 200,
+    'body': JSON.stringify({
+      message: event.params.message,
+    }),
+  };
+  return callback(null, response)
+}
+
+const errorHandler = (code) => {
+  return (event) => {
+    return {
+      'statusCode': code,
+      'body': JSON.stringify({
+        message: `${code} error`,
+      }),
+    }
+  };
+};
+
+module.exports = {
+  test,
+  hello,
+  errorHandler,
+}
